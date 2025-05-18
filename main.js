fb1bfbde449c90f6aeffa67a1d0a0c94cc9a3ad0
DESKTOP-HVH1CSC\ASUS <ggapsang@gmail.com>
2025-05-19 00:13:41 +0900

    JS 파일 추가

---
+++ main.js
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright 2023 EunneyBoogy Studio
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * it under the terms of the Apache License, Version 2.0
+ * as published by Sanghyun
+ * 
+ */
+
+/**
+ * 바닐라 자바스크립트로 TO DO LIST 구현
+ * 
+ * 1) 체크박스 클릭 로직
+ *  - 빈 체크박스 클릭시 체크박스 체크
+ *      - 체크박스 체크될 경우 할 일 부분에 줄 긋기   
+ *  - 체크된 체크박스 클릭시 체크박스 체크 해제
+ *      - 체크박스 체크 해제될 경우 할 일 부분에 줄 긋기 제거
+ * 2) 삭제 버튼 클릭 로직
+ *  - 삭제 버튼 클릭시 해당 할일 목록 삭제
+ * 3) 추가 버튼 클릭 로직
+ *  - 추가 버튼 클릭시 빈 할일 목록 추가
+ * 4) 할일 목록 클릭 로직
+ *  - 할일 목록 클릭시 해당 할일 목록 수정
+ */
+
