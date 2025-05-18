f581273f0642e7369eb73c32c1bd42156f7bae81
DESKTOP-HVH1CSC\ASUS <ggapsang@gmail.com>
2025-05-19 01:15:40 +0900

    add : eventListener

--- main.js
+++ main.js
@@ -24,3 +24,43 @@
  *  - 할일 목록 클릭시 해당 할일 목록 수정
  */
 
+document.addEventListener("DOMContentLoaded", function() {
+    const addToDoListButton = document.querySelector('.format-list-bulleted');
+    const toDoContainer = document.querySelector('.desktop');
+
+    console.log('To Do List App Loaded');
+
+    addToDoListButton.addEventListener('click', function() {
+    createToDoItem('할 일 내용');
+    });
+
+});
+
+function createToDoItem(content) {
+
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('todolist-undoe-line');
+
+    // HTML 구조 생성
+    todoItem.innerHTML = `
+        <div class="overlap-group">
+            <div class="todolist-contents">${content}</div>
+            <img class="todolist-chkbutton" src="img/ToDoList_chkbutton_unchecked.png" />
+            <img class="delete-forever" src="img/Delete_forever.png" />
+        </div>
+    `;
+
+    toDoContainer.appendChild(todoItem);
+
+    const extistingItems = document.querySelectorAll('.todolist-do-line, .todolist-undoe-line');
+    const itemCount = extistingItems.length;
+
+    const baseTop = 308;
+    const itemHeight = 44;
+
+    todoItem.style.position = 'absolute';
+    todoItem.style.top = `${baseTop + ((itemCount - 1) * itemHeight)}px`;
+    todoItem.style.left = '300px'; // 기존 항목과 동일한 왼쪽 위치
+    
+    console.log(`새 할 일 "${text}" 추가됨`);
+}
